66cd697dc3db80e471042f104b370972da7b6b97
yoonsun9128 <yoonsun9128@gmail.com>
2022-10-28 16:26:33 +0900

    articles app made

--- DRF_djanog_advance/urls.py
+++ DRF_djanog_advance/urls.py
@@ -1,8 +1,14 @@
 
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('user/', include('users.urls')),
+    path('api/token/', TokenObtainPairView.as_view()),
+    path('api/token/refresh/', TokenRefreshView.as_view()),
+
 ]
